e778e7b0ffff66fd27b34745ae674fc582d0a1d8
Hongli Xu <wu402a8zc7@gmail.com>
2019-12-23 15:24:14 -0500

    setup devise

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,11 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 
 
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+//= require rails-ujs
+//= require activestorage
+//= require bootstrap
 //= require jquery
-//= require bootstrap-sprockets
